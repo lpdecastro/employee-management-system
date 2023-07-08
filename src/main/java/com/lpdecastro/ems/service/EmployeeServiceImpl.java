efa85309f56fb1ddcab5b868f7010912369112f1
Liandre John de Castro <liandrejohn_dc88@yahoo.com>
2023-07-09 05:54:41 +0800

    Added Employee service

---
+++ src/main/java/com/lpdecastro/ems/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.lpdecastro.ems.service;
+
+import com.lpdecastro.ems.entity.Employee;
+import com.lpdecastro.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        return employee.orElse(null);
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
