9a5d844e34a32ed450174fada4f8be44f0adb6b8
Liandre John de Castro <liandrejohn_dc88@yahoo.com>
2023-07-09 06:59:45 +0800

    Added MVC for get all employees

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+USE `employee_directory`;
+
+INSERT INTO `employee`
+VALUES (1, "testFirstName1", "testLastName1", "test1@yopmail.com"),
+       (2, "testFirstName2", "testLastName2", "test2@yopmail.com"),
+       (3, "testFirstName3", "testLastName3", "test3@yopmail.com"),
+       (4, "testFirstName4", "testLastName4", "test4@yopmail.com"),
+       (5, "testFirstName5", "testLastName5", "tes5t@yopmail.com");
